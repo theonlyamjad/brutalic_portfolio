98e7176c28d9cc7044bd907ffaed55bf3c41ba6d
theonlyamjad <amjadmessi2004@gmail.com>
2025-11-17 14:04:49 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -93,6 +94,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics/>
     </html>
   );
 }
--- components/Icondecor.tsx
+++ components/Icondecor.tsx
@@ -5,7 +5,7 @@ export default function IconDecor() {
     <svg
       viewBox="0 0 100 42"
       xmlns="http://www.w3.org/2000/svg"
-      className="w-10 h-auto md:w-14 xl:w-24"
+      className="w-10 h-auto md:w-14 xl:w-24" 
       fill="none"
     >
       <path d="M6.42 0L0.06 6.36L34.81 41.12L41.18 34.75L6.42 0Z" fill="#e9f294" />
